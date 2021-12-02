f2fcc2ce276ef411467c6308955e4bced8b4fe5d
Colin Mollenhour <colin@mollenhour.com>
2021-12-02 10:57:29 -0500

    Replace reference to old url that is no longer active.

--- code/Source/Serviceurl.php
+++ code/Source/Serviceurl.php
@@ -12,7 +12,7 @@ class ShipStream_Test_Source_Serviceurl implements Plugin_Source_Interface
     public function getOptions(Plugin_Abstract $plugin)
     {
         return [
-            ['label' => 'whatismyipaddress.com (insecure)', 'value' => 'http://bot.whatismyipaddress.com'],
+            ['label' => 'ipinfo.io (insecure)', 'value' => 'http://ipinfo.io/ip'],
             ['label' => 'ipify.org (secure)', 'value' => 'https://api.ipify.org'],
         ];
     }
